365de8c94d902c7f73840bf33decbebf3e0b6429
Egor Spirin <spirin.egor@mail.ru>
2021-06-16 17:41:27 +0300

    Disable mode logic

--- src/main.py
+++ src/main.py
@@ -3,9 +3,7 @@ import os
 
 from src.bot_controller import BotController
 
-MODE = "production"
 TELEGRAM_BOT_TOKEN = "TELEGRAM_BOT_TOKEN"
-TELEGRAM_TEST_BOT_TOKEN = "TELEGRAM_TEST_BOT_TOKEN"
 
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s",
@@ -14,10 +12,9 @@ logging.basicConfig(
 
 
 if __name__ == "__main__":
-    __token_key = TELEGRAM_BOT_TOKEN if MODE == "production" else TELEGRAM_TEST_BOT_TOKEN
-    __token = os.environ.get(__token_key)
+    __token = os.environ.get(TELEGRAM_BOT_TOKEN)
     if __token is None:
-        print(f'can\'t find token for bot in env variable "{__token_key}"')
+        print(f'can\'t find token for bot in env variable "{TELEGRAM_BOT_TOKEN}"')
     else:
         __bot_controller = BotController(__token)
         __bot_controller.start()
